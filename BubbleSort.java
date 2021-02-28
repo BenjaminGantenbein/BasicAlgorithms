b578e3b3d1c9cfce6bdf1fda3ccf401497395bf5
Ben <79652053+BenjaminGantenbein@users.noreply.github.com>
2021-02-28 20:38:04 +0100

    Add files via upload

---
+++ BubbleSort.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+
+public class BubbleSort{
+
+
+  public static int[] bubblesort(int[] array){
+
+      for(int i= 0; i < array.length; i++){
+        for(int j = i; j < array.length; j++){
+            if(array[i] > array[j]){
+              int temp = array[i];
+              array[i] = array[j];
+              array[j] = temp;
+            }
+
+        }
+      }
+      return array;
+
+
+  }
+
+
+
+  public static void main(String[] args){
+
+
+          int[] array = {900, 1, 3, 4, 6, 72, -7, 9};
+
+
+          System.out.println(Arrays.toString(bubblesort(array)));
+
+
+  }
+
+}
