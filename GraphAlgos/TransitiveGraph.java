8ebf55bf1b5dfa8fe166940c28f610417417f1c1
Ben <79652053+BenjaminGantenbein@users.noreply.github.com>
2022-01-08 13:58:52 +0100

    Create TransitiveGraph.java

---
+++ GraphAlgos/TransitiveGraph.java
@@ -0,0 +1,13 @@
+	public static boolean isTransitiveRelation() {
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				for (int k = 0; k < n; k++) {
+					if (matrix[i][j] && matrix[j][k] && !matrix[i][k]) {
+						return false;
+					}
+				}
+			}
+		}
+		return true;
+	}
